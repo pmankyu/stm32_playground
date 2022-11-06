983b4438b10063c0cf50225455fdae3fac194f5a
Park Man Kyu <pmankyu@gmail.com>
2022-11-06 17:08:46 +0900

    modify for nucleo-f103rb

--- ledblink_bare-metal/main.c
+++ ledblink_bare-metal/main.c
@@ -3,30 +3,33 @@
 // register address
 #define RCC_BASE      0x40021000
 #define GPIOC_BASE    0x40011000
+#define GPIOA_BASE    0x40010800
+
 #define RCC_APB2ENR   *(volatile uint32_t *)(RCC_BASE   + 0x18)
 #define GPIOC_CRH     *(volatile uint32_t *)(GPIOC_BASE + 0x04)
 #define GPIOC_ODR     *(volatile uint32_t *)(GPIOC_BASE + 0x0C)
 
+#define GPIOA_CRH     *(volatile uint32_t *)(GPIOA_BASE + 0x04)
+#define GPIOA_ODR     *(volatile uint32_t *)(GPIOA_BASE + 0x0C)
+
+
 // bit fields
-#define RCC_IOPCEN   (1<<4)
+#define RCC_IOPAEN   (1<<2)
 #define GPIOC13      (1UL<<13)
+#define GPIOA5       (1UL<<5)
 
 
 void main(void)
 {
-	RCC_APB2ENR |= RCC_IOPCEN;
-	GPIOC_CRH   &= 0xFF0FFFFF;
-	GPIOC_CRH   |= 0x00200000;
+	RCC_APB2ENR |= RCC_IOPAEN;
+	GPIOA_CRH   &= 0xFF0FFFFF;
+	GPIOA_CRH   |= 0x00200000;
 
 	while(1)
 	{
-		GPIOC_ODR |=  GPIOC13;
+		GPIOA_ODR |=  GPIOA5;
 		for (int i = 0; i < 500000; i++); // arbitrary delay
-		GPIOC_ODR &= ~GPIOC13;
+		GPIOA_ODR &= ~GPIOA5;
 		for (int i = 0; i < 500000; i++); // arbitrary delay
 	}
 }
-
-
-
-	
